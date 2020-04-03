7d2c7f82b168a21b57918464e898ba7a59dc4236
imshubham27 <shubhsharma.2708@gmail.com>
2020-04-03 18:00:03 +0530

    updated

---
+++ Basic/py.py
@@ -0,0 +1,86 @@
+# import config
+# import os
+
+# print(os.getcwd())
+
+# def func(*names):
+#     print(names)
+#     t=(1,2,3,4)
+#     print(t[0])    
+#     t={1,2,3,4,5,21,1,2}
+#     print(t)
+
+
+
+# func("shubham","sharma")
+
+# print(config.a)
+
+# str = 'cold'
+
+# # enumerate()
+# list_enumerate = list(enumerate(str))
+# print('list(enumerate(str) = ', list_enumerate)
+
+# #character count
+# print('len(str) = ', len(str))
+
+# a="shubham"
+# b=","
+
+# c=b.join(a)
+
+# print(c)
+
+# a=set()
+
+# a.update([1,4,3,1,34,1,34,234])
+
+# print(a)
+
+# d= {7:'c',1:'a',2:'b',3:'d',4:'e'}
+
+# d[6]='f'
+
+# print(d)
+
+# d.pop(3)
+
+# print(d)
+
+# l=[1,2,3,4,5,6]
+
+# l.pop(4)
+
+# print("LIST",l)
+
+# print(d.values())
+
+# for i in d.values():
+#     print(i)
+
+# lis=[x**2 for x in range(0,10)]
+
+# print("LIST",lis)
+
+# print(sorted(l))
+
+
+# f= open('python.txt')
+
+# f.read(6)
+
+# with open('python.txt','w') as f:
+#     f.write("Hi I am shubham")
+
+# f= open('python.txt')
+
+# print(f.read())
+
+# f.tell()
+
+# f.seek(0)
+
+# f.readline()
+
+import os
---
+++ Basic/tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+
+# func("shubham","sharma")
+
+# print(config.a)
+
+l=[1,2,3,4,5]
+
+a,b,c,d,e=l 
+print(a)
