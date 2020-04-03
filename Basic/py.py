6bb856d774ad8c66bcba18db3868e2feeeb4b60b
imshubham27 <shubhsharma.2708@gmail.com>
2020-04-03 19:22:52 +0530

    Updated

--- Basic/py.py
+++ Basic/py.py
@@ -84,3 +84,56 @@
 # f.readline()
 
 import os
+
+print(os.getcwd())
+
+os.chdir('D:\GIT\Python')
+
+print(os.getcwd())
+
+print(os.listdir())
+
+os.mkdir('TEST')
+
+os.listdir()
+
+os.remove('TEST')
+
+os.listdir()
+
+a=[1,2,3,0]
+
+for i in a:
+    try:
+        b=5/i
+        print(b)
+    except:
+        print("NO")
+    finally:
+        print("HIIIII")
+
+class Parrot:
+
+    # class attribute
+    species = "bird"
+
+    # instance attribute
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+# instantiate the Parrot class
+blu = Parrot("Blu", 10)
+woo = Parrot("Woo", 15)
+
+# access the class attributes
+print("Blu is a {}".format(blu.__class__.species))
+print("Woo is also a {}".format(woo.__class__.species))
+
+# access the instance attributes
+print("{} is {} years old".format( blu.name, blu.age))
+print("{} is {} years old".format( woo.name, woo.age))
+
+A
+
+
